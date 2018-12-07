7eb7bc2d22fef027aa9d52b3dcd0891904871256
ytu94 <453980450@qq.com>
2018-12-07 17:31:43 +0800

    添加课程的接口

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var articleRouter = require('./routes/article');
+// var usersRouter = require('./routes/users');
+// var articleRouter = require('./routes/article');
 
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var mysql      = require('mysql');
 let index = require('../controls/index');
-let func = require('../sql/func');
-var pool  = mysql.createPool( {
-  connectionLimit : 50,
-  host            : 'localhost',
-  user            : 'root',
-  password    : '261011',
-  database     : 'blog',
- multipleStatements : true  //是否允许执行多条sql语句
-});
+let course = require('../controls/course')
 
 
-router.get('/a', function(req, res, next) {
-  pool.getConnection((err, conn) => {
-    if(err) console.log(err)
-    conn.query('SELECT 1 + 1 AS solution', '', (err, response) => {
-      res.json({code: 200, msg: 'ok', goods: response});
-      console.log('asdsad', response)
-      conn.release()
-      if(err) return
-    })
-  })
-  // res.render('index', { title: 'Express' });
-});
-
+router.get('/courseList', course.getCouseList)
+router.post('/courseInfo', course.getCouseInfo)
 router.get('/all', index.getAll)
 router.get('/test', index.test)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ytu' });
 });
-
 module.exports = router;
