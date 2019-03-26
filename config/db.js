5893c5374512aaf23fcc13be8bf5ee8effdb9eb1
linyi <linyi@tsign.cn>
2019-03-26 11:21:25 +0800

    @feat 修改数据库name

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
-  connectionLimit : 50,
-  host            : 'localhost',
-  user            : 'root',
-  password    : '261011',
-  database     : 'ytu',
-  multipleStatements : true  //是否允许执行多条sql语句
+  connectionLimit: 50,
+  host: 'localhost',
+  user: 'root',
+  password: '261011',
+  database: 'ytuj_cn',
+  multipleStatements: true //是否允许执行多条sql语句
 };
